443ff9e31a62ea49ef66cfaf7b52bf641cb1d480
afeiship <1290657123@qq.com>
2021-10-14 13:23:09 +0800

    feat: map/mapIndexed

--- src/001-hello/src/Hello.kt
+++ src/001-hello/src/Hello.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>) {
-  println("Hello.")
+  val list = listOf<String>("hello", "world", "hello aric")
+  println("Hello world.")
+
+  println("==== array==== ")
+  list.forEach(System.out::println)
+
+  println("==== array map with index ==== ")
+  val list2 = list.mapIndexed { index, element -> "$index - $element" }
+
+
+  // 字符串模板
+  val a = 1;
+  val b = "abc";
+  val c = a.toString() + "---" + b;
+  val d = "$a --- $b"
+
+  println(c)
+  println(d)
+
+
+  println(list2)
 }
